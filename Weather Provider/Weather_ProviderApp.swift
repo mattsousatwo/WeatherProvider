73a2a5334273e28df37f80eaa9a3b9dbf6810591
Matthew Sousa <mattsousatwo@gmail.com>
2023-05-10 23:33:37 -0400

    Initial Commit

---
+++ Weather Provider/Weather_ProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ProviderApp.swift
+//  Weather Provider
+//
+//  Created by Matthew Sousa on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
